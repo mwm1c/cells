babb32983b812c23f4219fc9d3f8895e8d5c4992
mwm1c <zzk13872229834@163.com>
2022-02-08 22:54:12 +0800

    通过添加接口实现更复杂的细胞自动机--狐狸和兔子

--- src/field/View.java
+++ src/field/View.java
@@ -1,30 +1,36 @@
 package field;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import cell.Cell;
 
 public class View extends JPanel {
-	private static final long serialVersionUID = -5258995676212660595L;
+	private static final long serialVersionUID = -2417015700213488315L;
 	private static final int GRID_SIZE = 16;
-	private Field theField;
-	
+	private Field thefield;
+
 	public View(Field field) {
-		theField = field;
+		thefield = field;
 	}
 
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
-		for ( int row = 0; row<theField.getHeight(); row++ ) {
-			for ( int col = 0; col<theField.getWidth(); col++ ) {
-				Cell cell = theField.get(row, col);
-				if ( cell != null ) {
-					cell.draw(g, col*GRID_SIZE, row*GRID_SIZE, GRID_SIZE);
+		g.setColor(Color.gray);
+		for (int row = 0; row < thefield.getHeight(); row++) {
+			g.drawLine(0, row * GRID_SIZE, thefield.getWidth() * GRID_SIZE, row * GRID_SIZE);
+		}
+		for (int col = 0; col < thefield.getHeight(); col++) {
+			g.drawLine(col * GRID_SIZE, 0, col * GRID_SIZE, thefield.getHeight() * GRID_SIZE);
+		}
+		for (int row = 0; row < thefield.getHeight(); row++) {
+			for (int col = 0; col < thefield.getWidth(); col++) {
+				Cell cell = thefield.get(row, col);
+				if (cell != null) {
+					cell.draw(g, col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE);
 				}
 			}
 		}
@@ -32,24 +38,6 @@ public class View extends JPanel {
 
 	@Override
 	public Dimension getPreferredSize() {
-		return new Dimension(theField.getWidth()*GRID_SIZE+1, theField.getHeight()*GRID_SIZE+1);
+		return new Dimension(thefield.getWidth() * GRID_SIZE + 1, thefield.getHeight() * GRID_SIZE + 1);
 	}
-
-	public static void main(String[] args) {
-		Field field = new Field(10,10);
-		for ( int row = 0; row<field.getHeight(); row++ ) {
-			for ( int col = 0; col<field.getWidth(); col++ ) {
-				field.place(row, col, new Cell());
-			}
-		}
-		View view = new View(field);
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false);
-		frame.setTitle("Cells");
-		frame.add(view);
-		frame.pack();
-		frame.setVisible(true);
-	}
-
 }
